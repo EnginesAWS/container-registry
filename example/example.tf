2d8210790cfa9ebb50b860e8e134a67a21bc32b8
James Vodanovich <james@jvodan.com>
2022-06-10 12:01:59 +1000

    seperate out image build

--- example/example.tf
+++ example/example.tf
@@ -28,13 +28,5 @@ resource "aws_ecr_repository_policy" "demo-repo-policy" {
   }
   EOF
 }
-
-resource "null_resource" "push_images" {
-  provisioner "local-exec" {
-    command = "/tmp/script.sh"
-    interpreter = ["bash"]
-  }
-  depends_on = [
-    aws_ecr_repository_policy.demo-repo-policy
-  ]
 }
+
